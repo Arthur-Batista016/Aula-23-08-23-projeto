b5cd7a8b3cd664063e2e667a0b1a061081ef2321
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2023-09-27 20:58:16 -0300

    fix: arrumei erros do site

--- js/projeto.js
+++ js/projeto.js
@@ -306,50 +306,31 @@ function objeto(){
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    function cadastrar(){
-    
-        const produto = {
 
+    function Cadastrar(){
+        const produto = {
             nome: document.getElementById("prod_nome").value,
             marca: document.getElementById("prod_marca").value,
             preco: document.getElementById("prod_preco").value
-            
         }
-
-        //precisa colocar um id na ul//
-   //criar um novo elemento li//
-   const lista_item = document.createElement("li")
-   //adicionar conteudo nesse elemento//
-   //adciona os elementos dentro do innerhtml da lista, sem especificar que tipo de elemento eles são//
-   //lista_item.innerHTML += produto.nome//
-   //lista_item.innerHTML += produto.marca//
-   //lista_item.innerHTML += produto.preco//o + esquivale a mesma coisa escrita//
-   
-   const prod_nome = document.createElement("h3")
-   const prod_preco = document.createElement("h4")
-   const prod_marca = document.createElement("p")
-   
-   prod_nome.innerHTML = prod.nome
-   prod_marca.innerHTML = prod.marca
-   prod_preco.innerHTML = prod.preco
-   lista_item.appendChild(prod_nome)
-   lista_item.appendChild(prod_preco)
-   lista_item.appendChild(prod_marca)
-//elementos filhos sao de uma div//
-   
-   
-   //adicionar esse LI  no UL//
-    document.getElementById("lista_prod").appendChild(lista_item)
-
-    console.log(produto)
-    console.log(lista_item)
+    
+        const lista_item = document.createElement("li")
+        
+        const prod_nome  = document.createElement("h3")
+        
+        const prod_marca = document.createElement("h4") 
+        
+        const prod_preco = document.createElement("p")
+        
+        prod_nome.innerHTML = produto.nome
+        prod_marca.innerHTML = produto.marca
+        prod_preco.innerHTML = produto.preco
+        
+        lista_item.appendChild(prod_nome)
+        lista_item.appendChild(prod_marca)
+        lista_item.appendChild(prod_preco)
+        
+        document.getElementById("lista_prod").appendChild(lista_item)
+        console.log(produto)
+        console.log(lista_item)
     }
